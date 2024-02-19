079f23ca4e1190777bac57d813bba050ab4cdeb2
ctsevuga <ctsevuga@gmail.com>
2024-02-19 18:08:52 +0530

    Mongodb connection
    Mongodb connection string is updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect('mongodb+srv://sevchitra:san191995@cluster0.7etot7b.mongodb.net/karaikudishop?retryWrites=true&w=majority');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
