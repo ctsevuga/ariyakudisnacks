3830b08054af98296bcc27c5ea53d44687730416
ctsevuga <ctsevuga@gmail.com>
2024-02-20 17:31:08 +0530

    server call fix
    server call fix

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,7 +1,10 @@
 // export const BASE_URL =
-//   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
+export const BASE_URL = process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
 // export const BASE_URL = ''; // If using proxy
-export const BASE_URL = `$req.protocol}://$(req.get('host')}`; // production
+// if(process.env.NODE_ENV === "production"){
+//     BASE_URL = `$req.protocol}://$(req.get('host')}`
+//     } 
+// export const BASE_URL = `$req.protocol}://$(req.get('host')}`; // production
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
